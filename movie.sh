3153c1e50fdf1d43ca34168ee4ee6967a9fe1dee
Richard Bendli <93144317+richardbendli@users.noreply.github.com>
2024-05-06 16:07:26 +0100

    ---

--- movie.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# dependencies: fzf
-# path to the movies folder
-movies="/home/$USER/media/movies"
-
-menu=$(ls -a "$movies" | uniq -u | fzf --height=100% --reverse --header-first)
-
-exec nohup mpv "$movies"/"$menu">/dev/null 2>&1
